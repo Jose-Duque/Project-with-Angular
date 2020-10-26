fcfee6b360a87bf687ee604aa6ecd0cd8619a551
José Duque <cj_duque@hotmail.com>
2020-10-26 17:43:05 -0300

    created product-read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,17 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { BluevioletDirective } from './directives/blueviolet.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
 
 @NgModule({
   declarations: [
@@ -31,6 +41,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     BluevioletDirective,
     ForDirective,
     ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component,
   ],
   imports: [
     BrowserModule,
@@ -40,6 +52,15 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-crud',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-crud.component.css']
 })
 export class ProductCrudComponent implements OnInit {
-
-  constructor() { }
+  //atributo = 'Testando'
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create']);
+
+    console.log('Navegou para outra página !')
+  }
+
+  // fazerAlgo(): void {
+  //   console.log(this.atributo);
+  // } 
 
 }
